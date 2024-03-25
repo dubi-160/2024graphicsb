cad4516e2ffc8352271287c2745e3f848240c41f
dubi-160 <112918682+dubi-160@users.noreply.github.com>
2024-03-25 16:46:07 +0800

    week06

---
+++ week06-3_glm_obj_model/main.cpp
@@ -0,0 +1,28 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void drawmodel(void)
+{
+    if(!pmodel){
+    pmodel = glmReadOBJ("data/porsche.obj");
+    if(!pmodel) exit(0);
+    glmUnitize(pmodel);
+    glmFacetNormals(pmodel);
+    glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL );
+}
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH);
+    drawmodel();
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week06-3 glm obj model");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
