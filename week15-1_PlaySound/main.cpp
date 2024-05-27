aeb2c88c97943075919cc546216bfd6592b8677b
dubi-160 <112918682+dubi-160@users.noreply.github.com>
2024-05-27 16:45:31 +0800

    week15

---
+++ week15-1_PlaySound/main.cpp
@@ -0,0 +1,13 @@
+#include<windows.h>
+#include <GL/glut.h>
+int main()
+{
+   ///PlaySound("mykbeat.wav", NULL, SND_SYNC);
+   PlaySound("do.wav", NULL, SND_SYNC);
+   PlaySound("re.wav", NULL, SND_SYNC);
+   PlaySound("mi.wav", NULL, SND_SYNC);
+   PlaySound("fa.wav", NULL, SND_SYNC);
+   PlaySound("sol.wav", NULL, SND_SYNC);
+   PlaySound("la.wav", NULL, SND_SYNC);
+   PlaySound("la.wav", NULL, SND_SYNC);
+}
---
+++ week15-3_mouse_motion_glTranslatef_glRotatef/main.cpp
@@ -0,0 +1,40 @@
+#include <GL/glut.h>
+float angleX[15] = {}, angleY[15] = {};
+void display(){
+    ///glClearColor(1,1,0,1);///¶À¦â­I´º
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glTranslatef(angleX[14]/150.0, angleY[14]/150.0, 0);///·í²¾°Ê
+        glRotatef(angleX[0], 1,0,0);
+        glRotatef(angleY[0], 0,1,0);
+        glutSolidTeapot(0.3);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+int angleID = 0, oldX = 0, oldY = 0;
+void mouse(int button, int state, int x,int y){
+    oldX = x;
+    oldY = y;
+}
+void motion(int x, int y){
+    angleX[angleID] += x - oldX;
+    angleY[angleID] += y - oldY;
+     oldX = x;
+     oldY = y;
+    glutPostRedisplay();
+}
+void keyboard(unsigned char key, int x, int y){
+    if(key=='x') angleID = 14;
+}
+
+int main(int argc, char**argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week15-3");
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutMainLoop();
+}
